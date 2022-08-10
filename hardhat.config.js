e9dde245c8d6a0f14f75c535f6657865c643c9a6
Gaurav_Singh <gauravkarki038@gmail.com>
2022-08-10 18:39:39 +0530

    first commiedt

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,29 +10,16 @@ task("accounts", "Prints the List of Account", async (taskarg, hre) => {
 })
 
 const PRIVATE_KEY = process.env.NEXT_PUBLIC_PRIVATE_KEY
-const RINKEBY_RPC_URL = process.env.NEXT_PUBLIC_RINKEBY_RPC_URL
 const GOERLI_RPC_URL = process.env.NEXT_PUBLIC_GOERLI_RPC_URL
-const polygon_RPC_URL = process.env.NEXT_PUBLIC_RPC_URL
 
 module.exports = {
   solidity: "0.8.9",
   defaultNetwork: "goerli",
   networks: {
-    rinkeby: {
-      url: RINKEBY_RPC_URL,
-      accounts: [PRIVATE_KEY],
-      chainId: 4,
-      saveDeployments: true,
-    },
     localhost: {
       url: "http://127.0.0.1:8545/",
       // accounts: [Hardhat],
       chainId: 31337,
-    },
-    polygon: {
-      url: polygon_RPC_URL,
-      accounts: [PRIVATE_KEY],
-      chainId: 80001
     }
     ,
     goerli: {
